77959001a47bb5d626e42eaea73645aee522ca61
DanielRustrum <danieltrustrum@gmail.com>
2022-11-25 22:48:36 -0700

    Implement JSS

--- Source/app/pages/_app.tsx
+++ Source/app/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import React from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    const style = document.getElementById('server-side-styles')
+    if (style && style.parentNode) {
+      style.parentNode.removeChild(style)
+    }
+  }, [])
+  
   return <Component {...pageProps} />
 }
