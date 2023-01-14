ebc82a44e3ae70bd2bd4d1168d436f97a92d2e7b
DanielRustrum <danieltrustrum@gmail.com>
2023-01-14 11:39:13 -0700

    Write tests for reateLowRenderState

--- Source/app/tests/components/core/state.test.tsx
+++ Source/app/tests/components/core/state.test.tsx
@@ -171,8 +171,8 @@ describe('useToggleState', () => {
         return (
             <div>
                 {Array.from(states.keys()).map((key) => (
-                    <button key={key} onClick={() => toggleState(key)}>
-                        {key}: {states.get(key)}
+                    <button data-key={key} key={key} onClick={() => toggleState(key)}>
+                        {key}:{states.get(key)}
                     </button>
                 ))}
             </div>
@@ -180,14 +180,25 @@ describe('useToggleState', () => {
     }
 
     test('toggles the state of an item between primary and secondary', () => {
-        const {getByText} = render(<TestComponent />)
-        expect(getByText('item1: primary')).toBeDefined()
-        expect(getByText('item2: secondary')).toBeDefined()
-        expect(getByText('item3: secondary')).toBeDefined()
-        fireEvent.click(getByText('item2: secondary'))
-        expect(getByText('item1: secondary')).toBeDefined()
-        expect(getByText('item2: primary')).toBeDefined()
-        expect(getByText('item3: secondary')).toBeDefined()
+        const {container} = render(<TestComponent />)
+
+        let item1 = container.querySelector(`[data-key="item1"]`) as HTMLButtonElement
+        let item2 = container.querySelector(`[data-key="item2"]`) as HTMLButtonElement
+        let item3 = container.querySelector(`[data-key="item3"]`) as HTMLButtonElement
+        expect(item1.textContent).toBe("item1:primary")
+        expect(item2.textContent).toBe("item2:secondary")
+        expect(item3.textContent).toBe("item3:secondary")
+        
+        fireEvent.click(item2)
+
+        item1 = container.querySelector(`[data-key="item1"]`) as HTMLButtonElement
+        item2 = container.querySelector(`[data-key="item2"]`) as HTMLButtonElement
+        item3 = container.querySelector(`[data-key="item3"]`) as HTMLButtonElement
+        console.log(container.innerHTML)
+        expect(item1).not.toBeNull()
+        expect(item1.textContent).toBe("item1:secondary")
+        expect(item2.textContent).toBe("item2:primary")
+        expect(item3.textContent).toBe("item3:secondary")
     })
 })
 
@@ -195,144 +206,165 @@ import createLowRenderState from '../../../components/core/state/low-render'
 
 describe("createLowRenderState" , () => {
     test('Provider passes state to children', () => {
-      // Initialize the createLowRenderState function with an initial state
-      const initialState = {count: 0};
-      const [Provider, useState] = createLowRenderState(initialState);
+        // Initialize the createLowRenderState function with an initial state
+        const initialState = {count: 0}
+        const [Provider, useState] = createLowRenderState(initialState)
     
-      // Define a component that consumes the state using the useState hook
-      function Counter() {
-        const [state, setState] = useState(value => value);
-        return (
-          <div>
-            <span data-testid="count">{state.count}</span>
-            <button onClick={() => setState({count: state.count + 1})}>Increment</button>
-          </div>
-        );
-      }
+        // Define a component that consumes the state using the useState hook
+        function Counter() {
+            const [state, setState] = useState(value => value)
+            return (
+                <div>
+                    <span data-testid="count">{state.count}</span>
+                    <button onClick={() => setState({count: state.count + 1})}>Increment</button>
+                </div>
+            )
+        }
     
-      // Define a component that consumes a selected value of the state using the useState hook with a selector function
-      function CounterWithSelector() {
-        const [doubleCount, setDoubleCount] = useState<number>(state => state.count)
-        return (
-          <div>
-            <span data-testid="double-count">{`${doubleCount}`}</span>
-            <button onClick={() => setDoubleCount({count: (doubleCount as number) + 2})}>Increment</button>
-          </div>
-        );
-      }
+        // Define a component that consumes a selected value of the state using the useState hook with a selector function
+        function CounterWithSelector() {
+            const [doubleCount, setDoubleCount] = useState<number>(state => state.count)
+            return (
+                <div>
+                    <span data-testid="double-count">{`${doubleCount}`}</span>
+                    <button onClick={() => setDoubleCount({count: (doubleCount as number) + 2})}>Increment</button>
+                </div>
+            )
+        }
     
-      // Test that the Provider component correctly passes the state to the children components that consume the state using the useState hook
-      function TestProvider() {
-        const [state, setState] = useState();
-        return (
-          <Provider>
-            <Counter />
-            <CounterWithSelector />
-          </Provider>
-        );
-      }
-      const {getByTestId} = render(<TestProvider />);
-      expect(getByTestId('count').textContent).toBe('0');
-      expect(getByTestId('double-count').textContent).toBe('0');
-    });
+        // Test that the Provider component correctly passes the state to the children components that consume the state using the useState hook
+        function TestProvider() {
+            return (
+                <Provider>
+                    <Counter />
+                    <CounterWithSelector />
+                </Provider>
+            )
+        }
+        const {container} = render(<TestProvider />)
+        
+        let count = container.querySelector("[data-testid='count']") as HTMLSpanElement
+        let double_count = container.querySelector("[data-testid='count']") as HTMLSpanElement
+
+        expect(count.textContent).toBe('0')
+        expect(double_count.textContent).toBe('0')
+    })
     
     test('useState returns current state and setter function', () => {
-      // Initialize the createLowRenderState function with an initial state
-      const initialState = {count: 0};
-      const [Provider, useState] = createLowRenderState(initialState);
+        // Initialize the createLowRenderState function with an initial state
+        const initialState = {count: 0}
+        const [Provider, useState] = createLowRenderState(initialState)
     
-      // Test that the useState hook correctly returns the current state and a setter function to update the state
-      function TestUseState() {
-        const [state, setState] = useState();
-        return (
-          <div>
-            <span data-testid="state">{JSON.stringify(state)}</span>
-            <button onClick={() => setState({count: 10})}>Set State</button>
-          </div>
-        );
-      }
-      const {getByTestId} = render(
-        <Provider>
-          <TestUseState />
-        </Provider>
-      );
-      expect(getByTestId('state').textContent).toBe(JSON.stringify(initialState));
-      fireEvent.click(getByTestId('button'));
-      expect(getByTestId('state').textContent).toBe(JSON.stringify({count: 10}));
+        // Test that the useState hook correctly returns the current state and a setter function to update the state
+        function TestUseState() {
+            const [state, setState] = useState();
+            return (
+                <div>
+                <span data-testid="state">{JSON.stringify(state)}</span>
+                <button onClick={() => setState({count: 10})}>Set State</button>
+                </div>
+            )
+        }
+        const {container} = render(
+            <Provider>
+                <TestUseState />
+            </Provider>
+        )
+
+        let state = container.querySelector(`[data-testid="state"]`) as HTMLSpanElement
+        let button = container.querySelector(`button`) as HTMLButtonElement
+
+        expect(state.textContent).toBe(JSON.stringify(initialState))
+
+        fireEvent.click(button)
+
+        state = container.querySelector(`[data-testid="state"]`) as HTMLSpanElement
+        expect(state.textContent).toBe(JSON.stringify({count: 10}))
     })
 
     test('setter function updates state', () => {
         // Initialize the createLowRenderState function with an initial state
-        const initialState = {count: 0};
-        const [Provider, useState] = createLowRenderState(initialState);
-      
+        const initialState = {count: 0}
+        const [Provider, useState] = createLowRenderState(initialState)
+        
         // Test that the setter function returned by the useState hook correctly updates the state, and that the updates are correctly propagated to the children components that consume the state
         function TestSetState() {
-          const [state, setState] = useState<{count: number}>()
-          return (
-            <div>
-              <span data-testid="state">{JSON.stringify(state)}</span>
-              <button onClick={() => setState({count: (state.count as number) + 2})}>Increment</button>
-            </div>
-          );
+            const [state, setState] = useState<{count: number}>()
+            return (
+                <div>
+                    <span data-testid="state">{JSON.stringify(state)}</span>
+                    <button onClick={() => setState({count: (state.count as number) + 2})}>Increment</button>
+                </div>
+            )
         }
-        const {getByTestId} = render(
-          <Provider>
+
+        const {container} = render(
+            <Provider>
             <TestSetState />
-          </Provider>
-        );
-        fireEvent.click(getByTestId('button'));
-        expect(getByTestId('state').textContent).toBe(JSON.stringify({count: 1}));
-      });
-      
-      test('useState handles selector function', () => {
+            </Provider>
+        )
+
+        let state = container.querySelector(`[data-testid="state"]`) as HTMLSpanElement
+        let button = container.querySelector(`button`) as HTMLButtonElement
+
+        fireEvent.click(button)
+        expect(state.textContent).toBe(JSON.stringify({count: 2}))
+    })
+        
+    test('useState handles selector function', () => {
         // Initialize the createLowRenderState function with an initial state
-        const initialState = {count: 0};
-        const [Provider, useState] = createLowRenderState(initialState);
-      
+        const initialState = {count: 0}
+        const [Provider, useState] = createLowRenderState(initialState)
+        
         // Test that the useState hook correctly handles a selector function passed as an argument, and that the hook returns the selected value of the state
         function TestSelector() {
-          const [doubleCount, setDoubleCount] = useState(state => state.count * 2);
-          return (
-            <div>
-              <span data-testid="double-count">{`${doubleCount}`}</span>
-              <button onClick={() => setDoubleCount({count: (doubleCount as number) + 2})}>Increment</button>
-            </div>
-          );
-        }
-       
-        const {getByTestId} = render(
-            <Provider>
-              <TestSelector />
-            </Provider>
-          );
-          expect(getByTestId('double-count').textContent).toBe('0');
-          fireEvent.click(getByTestId('button'));
-          expect(getByTestId('double-count').textContent).toBe('2');
-        });
-        
-        test('useState handles partial state updates', () => {
-          // Initialize the createLowRenderState function with an initial state
-          const initialState = {count: 0};
-          const [Provider, useState] = createLowRenderState(initialState);
-        
-          // Test that the useState hook correctly handles updates to a partial state, and that the hook merges the partial updates with the existing state
-          function TestPartialState() {
-            const [state, setState] = useState();
+            const [doubleCount, setDoubleCount] = useState(state => state.count * 2)
             return (
-              <div>
-                <span data-testid="state">{JSON.stringify(state)}</span>
-                {/*@ts-expect-error  */}
-                <button onClick={() => setState({foo: 'bar'})}>Set Partial State</button>
-              </div>
-            );
-          }
-          const {getByTestId} = render(
+            <div>
+                <span data-testid="double-count">{`${doubleCount}`}</span>
+                <button onClick={() => setDoubleCount({count: (doubleCount as number) + 1})}>Increment</button>
+            </div>
+            )
+        }
+        
+        const {container} = render(
             <Provider>
-              <TestPartialState />
+                <TestSelector />
             </Provider>
-          );
-          fireEvent.click(getByTestId('button'));
-          expect(getByTestId('state').textContent).toBe(JSON.stringify({count: 0, foo: 'bar'}));
+        )
+
+        let double_count = container.querySelector(`[data-testid="double-count"]`) as HTMLSpanElement
+        let button = container.querySelector(`button`) as HTMLButtonElement
+        expect(double_count.textContent).toBe('0')
+        fireEvent.click(button)
+
+        double_count = container.querySelector(`[data-testid="double-count"]`) as HTMLSpanElement
+        expect(double_count.textContent).toBe('2')
+    })
+        
+    test('useState handles partial state updates', () => {
+        // Initialize the createLowRenderState function with an initial state
+        const initialState = {count: 0}
+        const [Provider, useState] = createLowRenderState(initialState)
+    
+        // Test that the useState hook correctly handles updates to a partial state, and that the hook merges the partial updates with the existing state
+        function TestPartialState() {
+        const [state, setState] = useState()
+        return (
+            <div>
+            <span data-testid="state">{JSON.stringify(state)}</span>
+            {/*@ts-expect-error  */}
+            <button onClick={() => setState({foo: 'bar'})}>Set Partial State</button>
+            </div>
+        )
+        }
+        const {container} = render(
+            <Provider>
+                <TestPartialState />
+            </Provider>
+        )
+        fireEvent.click(container.querySelector('button') as HTMLButtonElement)
+        let span = container.querySelector('[data-testid="state"]') as HTMLSpanElement
+        expect(span.textContent).toBe(JSON.stringify({count: 0, foo: 'bar'}))
     })
 })
